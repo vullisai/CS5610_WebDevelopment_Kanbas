76825cff3916b08e9ad55ddb51e9dabce55d9eb6
venkatsaivulli <vulli.v@northeastern.edu>
2024-02-21 09:28:09 -0500

    updating the link tag

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,4 +1,5 @@
 import ModuleList from "../Modules/List";
+import { Link } from "react-router-dom";
 import { FaDownload, FaArrowCircleRight, FaCrosshairs, FaChartBar, FaBullhorn, FaBell, FaTimes, FaMinusCircle } from "react-icons/fa";
 function Home() {
   return (
@@ -14,7 +15,7 @@ function Home() {
                                     <button className="btn btn-secondary  btn-block"><FaArrowCircleRight/> Import from Commons</button>
                                 </div>
                                 <div className="button-column">
-                                    <button className="btn btn-secondary"><FaCrosshairs/>
+                                 <button className="btn btn-secondary"><FaCrosshairs/>
                                         Choose Home
                                         Page</button>
                                 </div>
@@ -46,8 +47,8 @@ function Home() {
                                         <h5>To Do
                                         </h5>
                                         <br/>
-                                        <li className="list-group-item"><link><FaMinusCircle/>
-                                                Grade A1 - ENV + HTML <FaTimes/></link><br/>
+                                        <li className="list-group-item"><Link to={""}><FaMinusCircle/>
+                                                Grade A1 - ENV + HTML <FaTimes/></Link><br/>
                                             <span className="sub-text">100 points | Sep 18 at 11:59PM</span>
                                         </li>
                                     </ul>
